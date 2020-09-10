b72990453fc7486144245c9a8996da56e702150e
henrywoody <github@henrywoody.com>
2020-09-10 13:00:00 -0700

    Updates Heading link styling
    Updates the styling of anchors for Headings by adding an icon and moving
    the link to the end of the heading content.

--- client/src/Components/Heading/index.tsx
+++ client/src/Components/Heading/index.tsx
@@ -9,21 +9,38 @@ type HeadingProps = {
     level: HeadingLevelType;
     id?: string;
     className?: string;
+    children: React.ReactNode;
 }
 
 type HeadingTag = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 
 
-const Heading: React.FC<HeadingProps> = ({ level, id, className, children }) => {
+export default function Heading(props: HeadingProps) {
+    const { level, id, className, children } = props;
+
     const HComponent = `h${level}` as HeadingTag;
 
     const pathname = "/"; // TODO: find a way to get this dynamically
 
     return (
         <HComponent id={ id } className={ `heading ${className || ""} ${ !!id ? "linked" : ""}` }>
-            { !!id && <Link to={ `${pathname}#${id}` }>x</Link> } { children }
+            { children } { !!id && <Link to={ `${pathname}#${id}` }><LinkIcon/></Link> }
         </HComponent>
     )
 }
 
-export default Heading;
+
+function LinkIcon() {
+    return (
+        <div className="link-icon-container">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 391 500.72">
+                <g id="link-icon-container" data-name="Link Icon Container">
+                    <g id="link-icon-container-inner">
+                        <polygon className="hexagon" points="240 8.66 96.5 91.51 96.5 257.21 240 340.06 383.5 257.21 383.5 91.51 240 8.66"/>
+                        <polygon className="hexagon" points="151 160.66 7.5 243.51 7.5 409.21 151 492.06 294.5 409.21 294.5 243.51 151 160.66"/>
+                    </g>
+                </g>
+            </svg>
+        </div>
+    )
+}
